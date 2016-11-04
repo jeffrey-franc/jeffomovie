dd3581f9a2eda3747ed060288de25cbb0f8f7f4a
Jeffrey Franc <jef@medstatstudio.com>
2016-11-04 05:21:53 +0000

    Removes extraneous comments

--- media.py
+++ media.py
@@ -7,7 +7,7 @@ class Movie():
     __name__ = "Movie"
    
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        self.title = movie_title #self is needed if you will access as an instance variable
+        self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
